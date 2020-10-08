5b6b5040acbe834aa0223e2cdb0dd2e6b8a0ef93
hebermudezg <hebermudezg@unal.edu.co>
2020-10-08 18:10:08 -0500

    JSON
    cambios minimos

--- src/main/java/EscribirJSON.java
+++ src/main/java/EscribirJSON.java
@@ -8,23 +8,19 @@ public class EscribirJSON {
     public static String ruta = "src/main/resources/database/";
 
     // Funcion para guardar usuarios
-    public static void guardarUsuario(LinkedList<Usuario> usuario){
+    public static void guardarUsuario(LinkedList<Usuario> usuarios) {
         JSONObject UsuarioDatos = new JSONObject();
-        //UsuarioDatos.put("Documento", String.valueOf(usuario.get(1)));
-
-        UsuarioDatos.put("Documento", String.valueOf(usuario.Documento));
-        UsuarioDatos.put("Nombre", usuario.Nombre);
-        UsuarioDatos.put("Apellido", usuario.Apellido);
-        UsuarioDatos.put("Correo", usuario.Correo);
-        UsuarioDatos.put("Contraseña", usuario.Contraseña);
-        JSONObject UsuarioPerfil = new JSONObject();
-        UsuarioPerfil.put("Usuario", UsuarioDatos); //Esrbiendo el usuario
-
-
         JSONArray UsuarioLista = new JSONArray();
-        UsuarioLista.add(UsuarioPerfil);
-
-
+        for (Usuario usuario : usuarios) {
+            UsuarioDatos.put("Documento", String.valueOf(usuario.Documento));
+            UsuarioDatos.put("Nombre", usuario.Nombre);
+            UsuarioDatos.put("Apellido", usuario.Apellido);
+            UsuarioDatos.put("Correo", usuario.Correo);
+            UsuarioDatos.put("Contraseña", usuario.Contraseña);
+            JSONObject UsuarioPerfil = new JSONObject();
+            UsuarioPerfil.put("Usuario", UsuarioDatos); //Esrbiendo el usuario
+            UsuarioLista.add(UsuarioPerfil);
+        }
 
         try (FileWriter file = new FileWriter(ruta+"UsuariosJSON.json")) {
             file.write(UsuarioLista.toJSONString());
@@ -34,25 +30,9 @@ public class EscribirJSON {
         }
     }
 
+
+
+
+
 }
 
-/*
- JSONObject obj = new JSONObject();
-        obj.put("name", "mkyong.com");
-        obj.put("age", 100);
-
-        JSONArray list = new JSONArray();
-        list.add("msg 1");
-        list.add("msg 2");
-        list.add("msg 3");
-
-        obj.put("messages", list);
-
-        // Escribir el archivo
-        try (FileWriter file = new FileWriter("src/main/resources/database/usuarios.json")) {
-            file.write(obj.toJSONString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.print(obj);
- */
