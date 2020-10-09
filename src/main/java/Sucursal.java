3ee0afb722ce0885943848624de0730829023b6e
hebermudezg <hebermudezg@unal.edu.co>
2020-10-09 09:23:08 -0500

    surcur
    workspace

--- src/main/java/Sucursal.java
+++ src/main/java/Sucursal.java
@@ -1,29 +1,27 @@
-import java.util.LinkedList;
 
 public class Sucursal {
-    public int Codigo;
-    public String Ciudad;
-    public String Direccion;
-    public int Cant_Empleados;
-    public Empresa Empresa;
+    public int codigo;
+    public String ciudad;
+    public String direccion;
+    public int cantEmpleados;
+    public Empresa empresa;
 
-    public Sucursal(int codigo, String ciudad, String direccion, int cant_Empleados, Empresa empresa) {
-        this.Codigo = codigo;
-        this.Ciudad = ciudad;
-        this.Direccion = direccion;
-        this.Cant_Empleados = cant_Empleados;
-        this.Empresa = empresa;
-
-
-
+    public Sucursal(int codigo, String ciudad, String direccion, int cantEmpleados, Empresa empresa) {
+        this.codigo = codigo;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+        this.cantEmpleados = cantEmpleados;
+        this.empresa = empresa;
     }
+
     @Override
     public String toString() {
-        return "Sucursal " + "\n" +
-                "\t Codigo: " + Codigo + "\n" +
-                "\t Ciudad: " + Ciudad + "\n" +
-                "\t Direccion: " + Direccion + "\n" +
-                "\t Cantidad de empleados: " + Cant_Empleados + "\n" +
-                "\t ";
+        return "Sucursal{" +
+                "codigo=" + codigo +
+                ", ciudad='" + ciudad + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", cantEmpleados=" + cantEmpleados +
+                ", empresa=" + empresa +
+                '}';
     }
 }
